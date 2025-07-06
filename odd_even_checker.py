1e422dd4c2a92a406ba82ae929f124c1406b16ae
Tarun Tyagi <taruntyagi8@gmail.com>
2025-07-06 19:42:00 +0530

    Added message

--- odd_even_checker.py
+++ odd_even_checker.py
@@ -1,1 +1,5 @@
 ## Takes a number and tells if it's odd or even.
+
+## Create a function by name of oddEvent that takes a parameter of type integer
+## this function checks the integraer to be odd or even and then prints its oddd or even
+## call this function from your program with the following values - 2 , 10 , 13 , 0.989
