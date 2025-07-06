6549cc52c01c0c3a5c0f309492ebb8a5cfd63510
Tejas Tyagi <tejas@mywork.com>
2025-07-06 19:14:20 +0530

    Initial Commit

---
+++ TwoSum.py
@@ -0,0 +1,24 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+'''
+target = 6
+arr_nums = [3, 2, 4,5,3,4,5,]
+
+i = arr_nums[0]
+j = arr_nums[1]
+while i + j == target
+
