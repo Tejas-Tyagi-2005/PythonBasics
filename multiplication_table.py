e785d222918697aba81b454f94eba4a6327342f9
Tejas Tyagi <tejas@mywork.com>
2025-07-06 21:55:04 +0530

    Refactor multiplication table script for clarity and structure

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,1 +1,14 @@
-#Prints table of a number from 1 to 10.
+#Prints the multiplication table of any number from 1 to 10.
+
+num = 2 
+
+multiplicant = 1
+
+print(2*multiplicant)
+
+
+
+while(multiplicant<10):
+    multiplicant = multiplicant + 1
+    print(2*multiplicant)
+
