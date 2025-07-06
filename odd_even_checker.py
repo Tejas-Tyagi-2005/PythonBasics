ad08020732380f04e827f18812385e77f316f90f
Tejas Tyagi <tejas@mywork.com>
2025-07-06 20:00:44 +0530

    done

--- odd_even_checker.py
+++ odd_even_checker.py
@@ -3,3 +3,20 @@
 ## Create a function by name of oddEvent that takes a parameter of type integer
 ## this function checks the integraer to be odd or even and then prints its oddd or even
 ## call this function from your program with the following values - 2 , 10 , 13 , 0.989
+
+def oddEven(num : int):
+    if num % 2 == 0 :
+     print("et numero a even")
+    else :
+     print("et numaro a odda")
+
+
+
+oddEven(-2)
+
+oddEven(10)
+
+oddEven(13)
+
+oddEven(0.989)
+
