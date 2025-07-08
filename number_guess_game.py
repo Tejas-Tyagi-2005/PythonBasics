646a112f579fbb932d3380f1f0067ccd29a539ea
Tejas Tyagi <tejas@mywork.com>
2025-07-08 22:31:33 +0530

    number guessing game

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,1 +1,20 @@
-#User keeps guessing a random number until correct.
+#User keeps guessing a random number until correct
+
+
+user_num  = int(input("Guess a number between 1 and 10  :"))
+
+import random
+
+from matplotlib.pylab import rand
+rand_num = random.randint(1, 10)
+
+while user_num != rand_num :
+    if user_num < rand_num :
+        print("Too low , try again ")
+    else :
+        print("Too high ,try again ")
+    user_num = int(input("Guess a number between 1 and 10  :"))
+print("Congratulations! You guessed the number correctly.")
+
+
+
