c34e1e449200eac6b4b7f5c1db90e9a5cc554290
Tejas Tyagi <tejas@mywork.com>
2025-07-10 20:44:12 +0530

    Add newline and formatting improvements to factorial calculator script

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,1 +1,16 @@
 #Calculates factorial of a number using loop
+
+
+num = int(input("Enter a number to calculate its factorial :"))
+
+# factorial formula  = num! = num * (num -1) * (num -2) * ...* 1 
+
+
+result = 1 
+
+for i in range(1 , num +1 ):
+    result = result * i 
+    
+
+
+print("The factorial of ", num, "is" , result)
