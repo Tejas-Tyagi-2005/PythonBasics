b4c0eb6b47221d95393dd3e29cde1d5e554f6cd9
Tejas Tyagi <tejas@mywork.com>
2025-07-11 20:42:57 +0530

    Refactor prime checker script for improved readability and structure

--- prime_checker.py
+++ prime_checker.py
@@ -1,2 +1,16 @@
 # Ask the user to enter a number and check whether it is a prime number or not.
 # Print "Prime" if it is, else print "Not Prime".
+
+num = int(input("Enter a number to check if it is prime or not :"))
+
+
+if num<= 1 :
+    print("Et numero non primo")
+else:
+    for i in range(2 , num ):
+        if num % i == 0 :
+            print("et numero non primo")
+            break
+    else:
+
+      print("et numero e primo")
