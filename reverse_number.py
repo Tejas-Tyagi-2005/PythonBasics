ca65ea8988b236bb6abc82e254d94c1599cd335c
Tejas Tyagi <tejas@mywork.com>
2025-07-13 12:27:19 +0530

    Simplest program to reverse a number

--- reverse_number.py
+++ reverse_number.py
@@ -1,2 +1,9 @@
 # Ask the user to enter a number and print the reversed number.
 # Example: input 1234 → output 4321
+
+num = input("Enter a number to be reversed : ")
+reversed_num = num[::-1]
+print("Reversed number is:", reversed_num)
+
+
+
