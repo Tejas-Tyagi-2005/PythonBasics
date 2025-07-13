d68f948b578c3585489a0bbcf658dbaa6352cb9e
Tejas Tyagi <tejas@mywork.com>
2025-07-13 12:38:55 +0530

    simplest possible palaindrome checker

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,2 +1,12 @@
 # Ask the user to enter a string or number and check whether it is a palindrome.
 # Print "Palindrome" if it is, else print "Not Palindrome".
+
+
+user_str = input("Enter a string to check if it is a palandrome :")
+
+palindrome_check = user_str[::-1]
+
+if palindrome_check == user_str :
+    print("The string you entered is a palindrome ")
+else :
+    print("the string you entered is not a palindrome ")
