480ac4d55d68d8bfe31212862206c3adee3301cb
Tejas Tyagi <tejas@mywork.com>
2025-07-13 13:04:41 +0530

    Version with better looking user prompts

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,7 +1,7 @@
 # Ask the user to enter a string or number and check whether it is a palindrome.
 # Print "Palindrome" if it is, else print "Not Palindrome".
 
-
+print("\nThis programm checks if the input , is a palaindrome .\n \nEx - Nitin is a palindrome ,  Rohit is not \n ")
 user_str = input("Enter a string to check if it is a palandrome :")
 
 palindrome_check = user_str[::-1]
