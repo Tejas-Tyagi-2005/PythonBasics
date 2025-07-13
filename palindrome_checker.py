3d2848a57d78262ac9328a0d098f7a053bd03721
Tejas Tyagi <tejas@mywork.com>
2025-07-13 13:24:12 +0530

    Final version palindrome checker

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,8 +1,9 @@
 # Ask the user to enter a string or number and check whether it is a palindrome.
 # Print "Palindrome" if it is, else print "Not Palindrome".
 
-print("\nThis programm checks if the input , is a palaindrome .\n \nEx - Nitin is a palindrome ,  Rohit is not \n ")
-user_str = input("Enter a string to check if it is a palandrome :")
+print("\n palindrome refers to a string or number that is exactly the same when reversed , Ex - 121 = 121 , and Bob = Bob \n")
+print("\nThis programm checks if the input , is a palindrome .\n \nEx - Nitin is a palindrome ,  Rohit is not \n ")
+user_str = input("Enter a string to check if it is a palindrome :")
 
 palindrome_check = user_str[::-1]
 
