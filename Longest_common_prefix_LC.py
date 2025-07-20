0d8269d98aea20baa4f05679da087a2d5ea2f2b8
Tejas Tyagi <tejas@mywork.com>
2025-07-20 16:08:53 +0530

    FINAL

--- Longest_common_prefix_LC.py
+++ Longest_common_prefix_LC.py
@@ -1,20 +1,21 @@
-def longest_common_prefix(strs):
-    if not strs:
+def longest_common_prefix (strs):
+    if not strs :
         return ""
-
+    
     prefix = ""
+
 
     for i in range(len(strs[0])):
         char = strs[0][i]
-        for word in strs[1:]:
-            if i >= len(word) or word[i] != char:
-                return prefix
-        prefix += char
+        for words in strs[1:]:
+            if i >= len(words) or words[i] != char:
+                return prefix 
+        prefix = prefix + char 
 
-    return prefix
 
-user_input = input("Enter words separated by space: ")
+user_input = input("Enter strings seperated by spaces :")
 strs = user_input.strip().split()
 
+
 result = longest_common_prefix(strs)
-print("Longest common prefix:", result)
+print("the longest common prefix is :", result)
