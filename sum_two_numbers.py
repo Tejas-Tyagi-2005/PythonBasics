e5b073ab00ea9f5e71d97ef1442bea91525db012
Tejas Tyagi <tejas@mywork.com>
2025-08-12 10:06:28 +0530

    Add program to sum two user-input numbers

---
+++ sum_two_numbers.py
@@ -0,0 +1,13 @@
+# Write a Python program that takes two numbers as input from the user and prints their sum.
+
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+
+
+result = num1 + num2 
+
+print(f"The result of summation of {num1} and {num2} is {result}")
+
+# used f string for concatenation of variables inside print statemnts . 
+
+
