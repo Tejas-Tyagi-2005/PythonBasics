7c465840123c499871967e578594f54d8aeb7d48
Tejas Tyagi <tejas@mywork.com>
2026-03-24 19:34:12 +0530

    Add script to count how many times arr[i] is greater than arr[i+1]

---
+++ count_decreasing_pairs.py
@@ -0,0 +1,11 @@
+# Count how many times arr[i] > arr[i+1]
+
+arr = list(map(int,input("Enter the elements :").split()))
+
+count = 0 
+
+for i in range(len(arr)-1):
+    if arr[i]> arr[i+1]:
+        count += 1 
+
+print(count)
