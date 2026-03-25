d5d3081d55c2a0c85bd7ccc9c7d007057932f725
Tejas Tyagi <tejas@mywork.com>
2026-03-25 23:35:06 +0530

    sum valley

---
+++ sum_valleys.py
@@ -0,0 +1,12 @@
+# Find the sum of all elements which are smaller than both neighbors
+
+arr = list(map(int,input("Enter the elements of the array :").split()))
+
+total = 0 
+
+for i in range(1,len(arr)-1):
+    if arr[i]< arr[i+1] and arr[i]<arr[i-1]:
+        total += arr[i]
+
+print(total)
+
