86a586db824ea2773ed6832b1f8b320e4763c270
Tejas Tyagi <tejas@mywork.com>
2026-03-25 23:31:34 +0530

    repeat

---
+++ count_peaks(2).py
@@ -0,0 +1,12 @@
+# Count how many elements are greater than both neighbors
+
+arr = list(map(int,input("Enter the elements of the array :").split()))
+
+count = 0 
+
+for i in range(1,len(arr)-1):
+    if arr[i]>arr[i+1] and arr[i]> arr[i-1]:
+        count += 1 
+
+
+print(count)        
