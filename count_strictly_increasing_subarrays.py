a0982048945770af10a60211027f2054fe1cc639
Tejas Tyagi <tejas@mywork.com>
2026-03-25 19:54:37 +0530

    Add script to count strictly increasing subarrays in an array

---
+++ count_strictly_increasing_subarrays.py
@@ -0,0 +1,21 @@
+# Count how many strictly increasing subarrays exist in the array
+
+arr = list(map(int,input("Enter the elements of the array :").split()))
+
+current = 1 
+
+count = 1
+
+for i in range(len(arr)-1):
+    if arr[i] < arr[i+1]:
+        current += 1
+    else :
+        current = 1 
+
+    count += current 
+
+
+print(count)
+
+
+  
