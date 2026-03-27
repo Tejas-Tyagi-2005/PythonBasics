0c6934fc6f282d240bd5fcce9682e3e13532bcfa
Tejas Tyagi <tejas@mywork.com>
2026-03-27 13:26:26 +0530

    Add ten function

---
+++ Functions.py/add_ten.py
@@ -0,0 +1,11 @@
+# Write a function that takes a number n  
+# and returns n + 10
+
+def add10(n):
+    n = int(input("Enter the number n :"))
+    return  n + 10 
+    result = n + 10 
+    print(result)
+
+add10(6)
+
